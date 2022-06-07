dc7dd1abaf5df28bf592720d0829e0162beaa290
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-07 16:21:09 -0700

    exclaim.js

---
+++ exclaim.js
@@ -0,0 +1,18 @@
+function exclaim(str) {
+    let newStr = ""
+
+    for (let i = 0; i < str.length; i++){
+if (str[i] === "?" || str[i] === "."){
+    newStr = newStr + "!"
+} else {
+    newStr = newStr  + str[i]
+}
+
+
+}  
+
+    return newStr
+}
+
+console.log(exclaim("What are you doing? Are you a fool?")) // What are you doing! Are you a fool!
+console.log(exclaim("This is fine.")) // This is fine!
