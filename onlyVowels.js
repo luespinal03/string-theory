581d56e2ae2f9902a0118af05f49959adcbcf716
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-08 11:15:02 -0700

    onlyVowels

---
+++ onlyVowels.js
@@ -0,0 +1,44 @@
+// doing it with !== and &&'s
+
+
+// function vowels(str) {
+//     let newStr = ""
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] !== "a" && str[i] !== "e" && str[i] !== "i" && str[i] !== "o" && str[i] !== "u") {
+//             // newStr = newStr + ""
+//         } else {
+//             newStr = newStr + str[i]
+//         }
+
+//     }
+//     return newStr
+// }
+
+// console.log(vowels("luis espinal"))
+// console.log(vowels("quickly"))
+// console.log(vowels("peter parker"))
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+// doing it with === and ||'s instead
+
+function vowels(str) {
+    let newStr = ""
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
+            newStr = newStr + str[i]
+        } else {
+            newStr = newStr + ""
+        }
+
+    }
+    return newStr
+}
+
+console.log(vowels("luis espinal"))
+console.log(vowels("quickly"))
+console.log(vowels("peter parker"))
