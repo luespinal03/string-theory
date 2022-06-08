ef421921e2d07498d172a86190a87eeedccd494a
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-06-08 10:32:28 -0700

    reverse.js

--- reverse.js
+++ reverse.js
@@ -1,16 +1,3 @@
-// let string1 = ["colin"]
-// let string2 = ["mesuara"]
-
-// string1.reverse()
-// // // string2.reverse()
-
-// console.log(string1.reverse())
-
-// for (let i = 0; i < string1.length; i++) {
-//     console.log(string1[i])
-// }
-
-
 
 function reverseString(str){
     return str.split("").reverse().join("")
